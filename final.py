c36a9773b4b0f5e1b79c55edfc38d621a6d983f3
HD.Giang <hoangducgiang250401@gmail.com>
2021-12-08 15:17:24 +0700

    final

--- final.py
+++ final.py
@@ -2,48 +2,8 @@ import cv2
 import numpy as ny
 import xml.etree.ElementTree as ET
 
-def moreThanZero(int1, int2):
-    if ((int1 - int2) > 0):
-        return int1 - int2
-    return int2 - int1
-
-def result(array1,array2):
-    
-    resultArray = []
-    #(0,3) (2,1)
-    for solution, answer in zip(array1, array2):
-        left_x = max(solution[0], answer[0])
-        top_y = min(solution[1], answer[1])
-        right_x = min(solution[2], answer[2])
-        bottom_y = max(solution[3], answer[3])
-
-        intersectionArea = moreThanZero(left_x, right_x) * moreThanZero(top_y, bottom_y)
-
-        solutionArea = moreThanZero(solution[0], solution[3]) * moreThanZero(solution[1], solution[2])
-
-        answerArea = moreThanZero(answer[0], answer[3]) * moreThanZero(answer[1], answer[2])
-        
-        resultArray.append(intersectionArea / float(solutionArea + answerArea- intersectionArea))
-
-    return resultArray
-
-def final(a,c):
-    # a.sort(key=lambda x: x[1], reverse=False)
-    # c.sort(key=lambda x: x[1], reverse=False)
-    resultArray = result(a,c)
-
-    resultValue = 0
-    
-    for ite in resultArray:
-        resultValue += ite   
-
-    resultValue /= len(resultArray)
-
-    return resultValue
-
 def processImage(img,morph_size):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # blur = cv2.GaussianBlur(gray, (9,9), 0)
     thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 10)
     kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, morph_size)
     thresh = cv2.dilate(thresh, kernel, iterations=1)
@@ -79,12 +39,9 @@ def get_htb(boxes):
 
 def lineSpacing(boxes):
     boxes.sort(key=lambda x: x[1], reverse=False)
-    # print(boxes)
-    MaxLine = 1000
+    MaxLine = 0
     for i in range(len(boxes)-1):
-        print(MaxLine)
-        # print((boxes[i + 1][1] - boxes[i][1]))
-        if MaxLine > (boxes[i + 1][1] - boxes[i][1]) > 10:
+        if MaxLine < (boxes[i + 1][1] - boxes[i][1]) > 10:
             MaxLine = (boxes[i + 1][1] - boxes[i][1])
     return MaxLine
         
@@ -97,17 +54,34 @@ def getCorrection(thresh,with_ = 3,height_ = 1):
     chieucaotb = get_htb(boxes)
     return [chieucaotb,line_spacing]
 
-def testDNA(box1,box2,boxes):
+def testDNA(box1,box2):
     (x, y, w, h) = box1
     (x1, y1, w1, h1) = box2
     if (x+w >= x1) and (x1+w1 >= x) and (y+h >= y1) and (y1+h1 >= y):
         return True
     return False
 
+def final(results,resultsXml):
+    lenArrXml = len(resultsXml)
+    lenArrRs = len(results)
+    lenArrMin = min(lenArrRs,lenArrXml)
+    for i in range(lenArrMin):
+        if testDNA(resultsXml[i],results[i]):
+            (x, y, x1, y1) = resultsXml[i]
+            (x2, y2, x3, y3) = results[i]
+            areaXml = (x1 - x)*(y1 - y)
+            areaRs = (x3 - x2)*(y3 - y2)
+            w = min(x1,x3) - max(x,x2)
+            h = min(y1,y3) - max(y,y2)
+            areaOverlap = w*h
+            print("vị trí ",i,"có độ chính xác là : ",areaOverlap/(areaXml + areaRs - areaOverlap))
+        else:
+            print("vị trí ",i,"có độ chính xác là : 0")
+    return lenArrMin
 def getAllOverlaps(boxes,bounds, index):
     overlaps = []
     for i in range(len(boxes)):
-            if testDNA(bounds, boxes[i],boxes):
+            if testDNA(bounds, boxes[i]):
                 overlaps.append(boxes[i])
     return overlaps
 
@@ -117,31 +91,23 @@ def clearBoxes(boxes,r,d):
     while not finished:
         finished = True
 
-        index = len(boxes)-1 ## lấy độ dài vong lặp
+        index = len(boxes)-1
         while index >=0:
-            # print(len(boxes))
-            # print(index)
-            # print("befor:",boxes[index])
             (x, y, w, h) = boxes[index]
             x -= r
             y -= d
             w += 2*r
             h += 2*d
-            # print("after : ",boxes[index])
             overlaps = getAllOverlaps(boxes,(x, y, w, h),index)
-            # print("box overlaps: ",overlaps)
             con = []
             for box in overlaps:
                 (x1,y1,w1,h1) = box
-                # print("box",box)
                 tl = [x1,y1]
                 br = [x1 + w1, y1 + h1]
-                # print(tl,br)
                 con.append(tl)
                 con.append(br)
             con = ny.array(con)
             (x2,y2,w2,h2) = cv2.boundingRect(con)
-            # print("box merged: ",cv2.boundingRect(con))
             for ind in overlaps:
                     if ind in boxes:
                         boxes.remove(ind)
@@ -149,7 +115,6 @@ def clearBoxes(boxes,r,d):
             if len(overlaps) > 1:
                 finished = False;
                 break
-            # print(overlaps)
             index -= 1
 
     return boxes
@@ -170,24 +135,19 @@ def readXml(pathXml):
     return a
 
 if "BTL-DIP" == "BTL-DIP":
-    # imgname = "32"
-    imgname = "75094"
-    # imgname = "34"
+    imgname = "32"
     img = cv2.imread("data/public/" + imgname +".png")
     pathXml = 'data/public/' + imgname+'.xml'
     
     thresh = processImage(img,(1,1))
     correction = getCorrection(thresh)
-    # lấy thông số text
     height_text = correction[0]
     lineToLine = correction[1]
-    letter_spacing = round(height_text*1.5)
-    line_spacing =  lineToLine - round(height_text*1.6)
-    # xử lý theo thông số lấy được
+    letter_spacing = round(height_text*0.75)
+    line_spacing =  lineToLine - round(height_text*1.9)
     thresh2 = processImage(img,(round(height_text/2.5),1))
     boxes = find_Contours(thresh2)
     results = clearBoxes(boxes,letter_spacing,line_spacing)
-    # print(boxes)
     for box in results:
         (x, y, w, h) = box
         cv2.rectangle(img, (x, y), (x + w -1, y + h -1), (0, 0, 255), 1)
@@ -195,12 +155,10 @@ if "BTL-DIP" == "BTL-DIP":
     resultsXml = readXml(pathXml)
     for x in resultsXml:
         cv2.rectangle(img, (x[0], x[1]), (x[2], x[3]), (36,255,12), 1)
-    # for box in results:
-    #     print(box[2])
-    #     box[2] = box[0] + box[2]
-    #     box[3] = box[1] + box[3]
     results = [(box[0], box[1], box[2] + box[0], box[3] + box[1]) for box in results]
-    print(final(results,resultsXml))
+    results.sort(key=lambda x: x[0], reverse=False)
+    results.sort(key=lambda x: x[1], reverse=False)
+    final(results,resultsXml)
     cv2.imshow("thresh2",thresh2)
     cv2.imshow("thresh",thresh)
     cv2.imshow("img",img)
